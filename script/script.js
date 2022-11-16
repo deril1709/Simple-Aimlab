d422c96a650122bd22400c76c7bc4288f83d9238
deril1709 <punktator17@gmail.com>
2022-11-16 15:21:34 +0800

    change commit

---
+++ script/script.js
@@ -0,0 +1,43 @@
+let count = 1;
+var start = "";
+var end = "";
+var totalWaktu = "";
+
+//show target image (random place)
+function tampilkanGambar() {
+  var top = Math.random() * 400;
+  var left = Math.random() * 400;
+
+  start = new Date().getTime();
+
+  document.getElementById("target").style.height = 60 + "px";
+  document.getElementById("target").style.width = 60 + "px";
+  document.getElementById("target").src = "img/target.png";
+  document.getElementById("target").style.top = top + "px";
+  document.getElementById("target").style.left = left + "px";
+  document.getElementById("target").style.position = "relative";
+  document.getElementById("target").style.display = "block";
+}
+tampilkanGambar();
+
+//function to change the image
+document.getElementById("target").onclick = function () {
+  document.getElementById("target").src = "img/boom.png";
+  document.getElementById("target").style.height = 120 + "px";
+  document.getElementById("target").style.width = 120 + "px";
+  setTimeout(hideImage, 500);
+
+  // set total waktu
+  if (count <= 10) {
+    setTimeout(tampilkanGambar, 600);
+    count++;
+  } else {
+    alert("selesai");
+    end = new Date().getTime();
+    totalWaktu = (end - start) / 1000;
+    document.getElementById("totalWaktu").innerHTML = totalWaktu + "s";
+  }
+};
+function hideImage() {
+  document.getElementById("target").style.display = "none";
+}
